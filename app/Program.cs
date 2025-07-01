d7136b5a1388236a88f267b988ac04d1dc69885b
diegob-utn <nnbenavidesc@utn.edu.ec>
2025-07-01 12:34:27 -0500

    hello world - aiuda

--- app/Program.cs
+++ app/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 2 * 3; // This is a simple multiplication operation
+            Console.WriteLine(a);   // Output the result of the multiplication to the console
+
+            Console.WriteLine("Hello, World!"); // Print a greeting message to the console
         }
     }
 }
