0c5e891d8f169e0dc8bd2c35432bf8bb26b58f5f
diegob-utn <nnbenavidesc@utn.edu.ec>
2025-07-01 12:38:34 -0500

    agregar clases

---
+++ app/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    public class Producto
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Producto(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Product Name: {Name}, Price: {Price}");
+        }
+    }
+}
---
+++ app/Sucursal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    public class Sucursal
+    {
+                public string Location { get; set; }
+        public List<Producto> Products { get; set; }
+
+        public Sucursal(string location)
+        {
+            Location = location;
+            Products = new List<Producto>();
+        }
+
+        public void AddProduct(Producto product)
+        {
+            Products.Add(product);
+        }
+
+        public void DisplayProducts()
+        {
+            Console.WriteLine($"Products available at {Location}:");
+            foreach (var product in Products)
+            {
+                product.DisplayInfo();
+            }
+        }
+    }
+}
