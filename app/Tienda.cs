78b6eab9eb4b11d4c17f0750725610c59c0c1c49
diegob-utn <nnbenavidesc@utn.edu.ec>
2025-07-01 12:40:21 -0500

    agregar ventas- tienda

---
+++ app/Tienda.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+
+    public class Tienda
+    {
+        public string Name { get; set; }
+        public List<Sucursal> Sucursales { get; set; }
+
+        public Tienda(string name)
+        {
+            Name = name;
+            Sucursales = new List<Sucursal>();
+        }
+
+        public void AddSucursal(Sucursal sucursal)
+        {
+            Sucursales.Add(sucursal);
+        }
+
+        public void DisplaySucursales()
+        {
+            Console.WriteLine($"Sucursales of {Name}:");
+            foreach (var sucursal in Sucursales)
+            {
+                Console.WriteLine(sucursal.Location);
+            }
+        }
+    }
+}
